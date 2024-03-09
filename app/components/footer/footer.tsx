60b666957c257ad0e18a21edff1fbb8b810ae220
arda <arda@ardas-Mac-mini.local>
2024-03-10 00:19:38 +0300

    Update footer, Bugfix navbar

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -3,7 +3,17 @@ import "@/app/components/footer/footer.css";
 export default function Footer() {
   return (
     <main>
-
+      <div className="footer flex justify-center m-8 p-4 rounded-xl bg-slate-700 text-center items-center">
+        <a href="https://github.com/aquilarda" className="flex">
+          <img
+            src="github-mark-white.png"
+            alt="GitHub Icon"
+            width={40}
+            height={40}
+          />
+          <h1 className="px-2">GitHub</h1>
+        </a>
+      </div>
     </main>
   );
 }
--- app/components/navbar/navbar.tsx
+++ app/components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import "@/app/components/navbar/navbar.css";
 export default function Navbar() {
   return (
     <main>
-      <div className="navbar flex justify-evenly m-8 p-4 rounded-xl bg-slate-700">
+      <div className="navbar flex justify-evenly m-8 p-4 rounded-xl bg-slate-700 text-center items-center">
         <h1>
           <a href="/">Guess The Number</a>
         </h1>
