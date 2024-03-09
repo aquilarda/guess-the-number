a964dccb6badcad50cbe9fb3dc4c16e6e1256f47
arda <arda@ardas-Mac-mini.local>
2024-03-10 01:08:37 +0300

    Bugfix, Update UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
+"use client";
 import "@/app/home.css";
 
 export default function Home() {
+  function placeHolder() {
+    console.log("Hello, World!");
+  }
+
+
   return (
     <main>
       <div className="question-mark flex justify-center m-8 p-4 rounded-xl bg-slate-700 text-center items-center">
@@ -19,6 +25,33 @@ export default function Home() {
         >
           Guess!
         </button>
+      </div>
+      <div className="game-mode flex justify-evenly text-center items-center m-8 p-4 rounded-xl bg-slate-700">
+        <button
+          className="modeButton p-4"
+          id="easyModeButtonID"
+          onClick={placeHolder}
+        >
+          Easy
+        </button>
+        <button
+          className="modeButton p-4"
+          id="intermediateModeButtonID"
+          onClick={placeHolder}
+        >
+          Intermediate
+        </button>
+        <button
+          className="modeButton p-4"
+          id="hardModeButtonID"
+          onClick={placeHolder}
+        >
+          Hard
+        </button>
+      </div>
+      <div className="notes flex justify-evenly text-center items-center m-8 p-4 rounded-xl bg-slate-700">
+        Easy: 0 - 20 <br /> Intermediate: 0 - 100 <br /> Hard: 0 - 250 <br />{" "}
+        You have 5 chance to guess.
       </div>
     </main>
   );
